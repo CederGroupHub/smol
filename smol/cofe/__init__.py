3a0432c00f34069a2ee1c2878087617bb50ba71e
lbluque <lbluque@berkeley.edu>
2020-01-17 10:37:31 +0000

    finalizing style edits 2

--- smol/cofe/__init__.py
+++ smol/cofe/__init__.py
@@ -4,3 +4,7 @@ from .configspace.clusterspace import ClusterSubspace
 from .wrangler import StructureWrangler
 from .expansion import ClusterExpansion
 from .regression.estimator import CVXEstimator
+
+
+__all__ = ['ClusterSubspace', 'StructureWrangler', 'ClusterExpansion',
+           'CVXEstimator']
--- smol/cofe/configspace/__init__.py
+++ smol/cofe/configspace/__init__.py
@@ -3,3 +3,6 @@ from .orbit import Orbit
 from .supercell import ClusterSupercell
 from . import basis
 from .ewald import EwaldTerm
+
+
+__all__ = ['Cluster', 'Orbit', 'ClusterSupercell', 'basis', 'EwaldTerm']
