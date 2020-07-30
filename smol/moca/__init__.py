74eed8a220ad8fa2f14892535edda14b0c7549e6
lbluque <lbluque@berkeley.edu>
2020-07-29 22:29:01 -0700

    init files for nice imports

--- smol/moca/__init__.py
+++ smol/moca/__init__.py
@@ -5,13 +5,13 @@ The moca (pronounced mocha) holds implementations of classes used to run
 Monte Carlo simulations using Cluster Expansion Hamiltonians.
 """
 
-from __future__ import division
-
 from .processor.expansion import CEProcessor
 from .processor.ewald import EwaldProcessor
 from .processor.composite import CompositeProcessor
 from .ensemble.canonical import CanonicalEnsemble
 from .ensemble.semigrand import MuSemiGrandEnsemble, FuSemiGrandEnsemble
+from .sampler.metropolis import MetropolisSampler
 
 __all__ = ['CEProcessor', 'EwaldProcessor', 'CompositeProcessor',
-           'CanonicalEnsemble', 'MuSemiGrandEnsemble', 'FuSemiGrandEnsemble']
+           'CanonicalEnsemble', 'MuSemiGrandEnsemble', 'FuSemiGrandEnsemble',
+           'MetropolisSampler']
---
+++ smol/moca/sampler/__init__.py
@@ -0,0 +1,8 @@
+"""
+Implementation of Sampler classes, and other relevant classes for Markov Chain
+Monte Carlo Sampling
+"""
+
+from .container import SampleContainer
+
+__all__ = ['SampleContainer']
