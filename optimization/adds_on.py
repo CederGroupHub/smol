1a857a22d31c4633ac7ff3f9ca5d9b0f0bc1c643
Peichen Zhong <zhongpc@berkeley.edu>
2020-08-02 00:40:50 -0700

    0

---
+++ optimization/adds_on.py
@@ -0,0 +1,14 @@
+def cluster_properties(ce):
+    """
+    return max radius and number of atoms in cluster
+
+    input ce is cluster expansion class object
+    """
+    cluster_n = [0]
+    cluster_r = [0]
+    for sc in ce.symmetrized_clusters:
+        for j in range(len(sc.bit_combos)):
+            cluster_n.append(sc.bit_combos[j].shape[1])
+            cluster_r.append(sc.max_radius)
+    return np.array(cluster_n), np.array(cluster_r)  # without ewald term
+
