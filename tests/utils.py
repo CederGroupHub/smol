b7aba0bbd02155a98850a30d436fd9319c9b3ea5
lbluque <lbluque@berkeley.edu>
2020-08-07 15:19:03 -0700

    add tests/utils.py

---
+++ tests/utils.py
@@ -0,0 +1,24 @@
+"""
+A few testing utilities that may be useful to just import and run.
+Some of these are borrowed from pymatgen test scripts.
+"""
+
+import unittest
+import json
+import numpy.testing as nptu
+from monty.json import MontyDecoder, MSONable
+from monty.serialization import loadfn
+
+
+def assert_msonable(test_case, obj, test_if_subclass=True):
+    """
+    Tests if obj is MSONable and tries to verify whether the contract is
+    fulfilled.
+    By default, the method tests whether obj is an instance of MSONable.
+    This check can be deactivated by setting test_if_subclass to False.
+    """
+    if test_if_subclass:
+        test_case.assertIsInstance(obj, MSONable)
+    test_case.assertDictEqual(obj.as_dict(), obj.__class__.from_dict(
+        obj.as_dict()).as_dict())
+    json.loads(obj.to_json(), cls=MontyDecoder)
