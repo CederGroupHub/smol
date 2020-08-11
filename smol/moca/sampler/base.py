ccd547f193f56a4fd4be63625d21a0f06371b23f
lbluque <lbluque@berkeley.edu>
2020-08-11 16:51:16 -0700

    minor change in sampler progress message

--- smol/moca/sampler/base.py
+++ smol/moca/sampler/base.py
@@ -142,8 +142,9 @@ class Sampler(ABC):
 
         # Initialise progress bar
         nwalkers, nsites = self.samples.shape
-        desc = (f'Sampling {nwalkers} chains at {self.ensemble.temperature} K'
-                f' from a cell with {nsites} sites')
+        desc = (f'Sampling with {nwalkers} walkers at'
+                f'{self.ensemble.temperature} K from a cell with '
+                f'{nsites} sites.')
         with progress_bar(progress, total=nsteps, description=desc) as bar:
             for _ in range(nsteps // thin_by):
                 for _ in range(thin_by):
