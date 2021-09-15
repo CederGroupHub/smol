a1a3d703c34719f85a2c591dce44b8001e017771
qchempku2017 <664222956@qq.com>
2021-09-14 21:25:08 -0700

    cherry pick fix for cluster search by qchempku2017

--- smol/cofe/space/clusterspace.py
+++ smol/cofe/space/clusterspace.py
@@ -871,7 +871,9 @@ class ClusterSubspace(MSONable):
             exp_struct (Structure):
                 Structure with all sites that have partial occupancy.
             cutoffs (dict):
-                dict of cutoffs for cluster diameters {size: cutoff}
+                dict of cutoffs for cluster diameters {size: cutoff}.
+                Cutoff diameters must decrease with cluster size, otherwise
+                algorithm can not guarantee completeness of cluster subspace.
             symops (list of SymmOps):
                 list of symmetry operations for structure
             basis (str):
@@ -896,8 +898,11 @@ class ClusterSubspace(MSONable):
         orbits = {}
         new_orbits = []
         for nbit, site, sbasis in zip(nbits, exp_struct, site_bases):
-            new_orbit = Orbit([site.frac_coords], exp_struct.lattice,
-                              [list(range(nbit))], [sbasis], symops)
+            # Coordinates of point terms must stay in [0, 1] to guarantee
+            # correct math of the following algorithm.
+            new_orbit = Orbit(
+                [np.mod(site.frac_coords, 1)], exp_struct.lattice,
+                [list(range(nbit))], [sbasis], symops)
             if new_orbit not in new_orbits:
                 new_orbits.append(new_orbit)
 
@@ -908,12 +913,17 @@ class ClusterSubspace(MSONable):
         if len(cutoffs) == 0:  # return singlets only if no cutoffs provided
             return orbits
 
-        max_lp = max(exp_struct.lattice.abc) / 2
+        # Vector sum of a, b, c divided by 2.
+        # diameter + max_lp gives maximum possible distance from
+        # [0.5, 0.5, 0.5] prim centoid to a point in all enumerable
+        # clusters. Add 0.01 as a numerical tolerance grace.
+        max_lp = np.linalg.norm(np.sum(exp_struct.lattice.matrix, axis=0)) / 2
+        max_lp += 0.01
         for size, diameter in sorted(cutoffs.items()):
             new_orbits = []
-            neighbors = exp_struct.get_sites_in_sphere([0.5, 0.5, 0.5],
-                                                       diameter + max_lp,
-                                                       include_index=True)
+            neighbors = exp_struct.get_sites_in_sphere(
+                [0.5, 0.5, 0.5], diameter + max_lp, include_index=True)
+
             for orbit in orbits[size-1]:
                 if orbit.base_cluster.diameter > diameter:
                     continue
