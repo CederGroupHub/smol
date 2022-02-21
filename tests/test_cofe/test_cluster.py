934c3cc57b314f551210185805890951c9570392
lbluque <lbluque@berkeley.edu>
2022-02-21 14:05:51 -0800

    cluster pytests

--- tests/test_cofe/test_cluster.py
+++ tests/test_cofe/test_cluster.py
@@ -1,69 +1,56 @@
-import unittest
-import json
+import pytest
+from random import choices
 from itertools import combinations
 import numpy as np
-from pymatgen.core import Structure, Lattice
 from smol.cofe.space import Cluster
+from tests.utils import assert_msonable
 
 
-class TestCluster(unittest.TestCase):
-    def setUp(self) -> None:
-        self.lattice = Lattice([[3, 3, 0], [0, 3, 3], [3, 0, 3]])
-        species = [{'Li': 0.1, 'Ca': 0.1}] * 3 + ['Br']
-        coords = ((0.25, 0.25, 0.25), (0.75, 0.75, 0.75),
-                  (0.5, 0.5, 0.5), (0, 0, 0))
-        self.structure = Structure(self.lattice, species, coords)
+@pytest.fixture
+def cluster(structure):
+    num_sites = np.random.randint(2, 6)
+    sites = choices(structure.frac_coords, k=num_sites)
+    # add random integer multiples
+    for site in sites:
+        site += np.random.randint(-3, 3)
+    cluster = Cluster(sites, structure.lattice)
+    cluster.assign_ids(1)
+    return cluster
 
-        # Create cluster
-        c1 = np.array([0.25, 0.25, -1.75])
-        c2 = np.array([0.75, 0.75, -1.25])
-        c3 = np.array([0, 0, -2])
-        c4 = np.array([0.5, 0.5, -1.5])
-        self.cluster = Cluster([c1, c2, c3, c4], self.lattice)
-        self.cluster.assign_ids(1)
 
-    def test_from_sites(self):
-        clust1 = Cluster.from_sites(self.structure.sites)
-        self.assertEqual(clust1, self.cluster)
+def test_from_sites(structure):
+    assert Cluster(structure.frac_coords, structure.lattice) == Cluster.from_sites(structure.sites)
 
-    def test_size(self):
-        self.assertEqual(self.cluster.size, 4)
-        self.assertEqual(len(self.cluster), self.cluster.size)
 
-    def test_radius(self):
-        coords = self.lattice.get_cartesian_coords(self.cluster.sites)
-        radius = max([np.sum((i - j)**2) for i, j in combinations(coords, 2)])**0.5
-        self.assertEqual(self.cluster.diameter, radius)
+def test_size(cluster):
+    assert cluster.size == len(cluster)
+    assert len(cluster.sites) == cluster.size
 
-    def test_periodicity(self):
-        """Test periodicity of clusters."""
-        c1 = np.array([0.25, 0.25, 0.25])
-        c2 = np.array([0.75, 0.75, -1.25])
-        c3 = np.array([0.75, 0.75, 0.75])
-        c4 = np.array([0.25, 0.25, 2.25])
-        clust1 = Cluster([c1, c2], self.lattice)
-        clust2 = Cluster([c3, c4], self.lattice)
-        self.assertEqual(clust1, clust2)
 
-    def test_edge_case(self):
-        """Test edge case handling."""
-        c1 = np.array([0.25, 0.25, 0.25])
-        c2 = np.array([0, 0, 1])
-        c3 = np.array([0.25, 1.25, -0.75])
-        c4 = np.array([0, 1, 0])
-        clust1 = Cluster([c1, c2], self.lattice)
-        clust2 = Cluster([c3, c4], self.lattice)
-        self.assertEqual(clust1, clust2)
+def test_diameter(cluster):
+    coords = cluster.lattice.get_cartesian_coords(cluster.sites)
+    diameter = max([np.sum((i - j)**2) for i, j in combinations(coords, 2)])**0.5
+    assert diameter == cluster.diameter
 
-    def test_repr(self):
-        repr(self.cluster)
 
-    def test_str(self):
-        str(self.cluster)
+def test_periodicity(cluster):
+    new_sites = cluster.sites.copy()
+    new_sites += np.random.randint(-5, 5)
+    new_cluster = Cluster(new_sites, cluster.lattice)
+    assert new_cluster == cluster
 
-    def test_msonable(self):
-        d = self.cluster.as_dict()
-        self.assertEqual(self.cluster, Cluster.from_dict(d))
-        j = json.dumps(d)
-        json.loads(j)
 
+def test_edge_case(structure):
+    c1 = np.array([0.25, 0.25, 0.25])
+    c2 = np.array([0, 0, 1])
+    c3 = np.array([0.25, 1.25, -0.75])
+    c4 = np.array([0, 1, 0])
+    clust1 = Cluster([c1, c2], structure.lattice)
+    clust2 = Cluster([c3, c4], structure.lattice)
+    assert clust1 == clust2
+
+
+def test_msonable(cluster):
+    _ = repr(cluster)
+    _ = str(cluster)
+    assert_msonable(cluster)
