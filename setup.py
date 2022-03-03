6b0cdfca77186d7e64985488a3ce1482639d526b
lbluque <lbluque@berkeley.edu>
2022-03-03 14:34:41 -0800

    fix docs in setup.py

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ setup(
     extras_require={
         "docs": [
             "sphinx==4.4.0",
-            "pydata-sphinx-theme-0.8.0",
+            "pydata-sphinx-theme==0.8.0",
             "ipython==8.1.0",
             "nbsphinx==0.8.8",
             "nbsphinx-link==1.3.0",
