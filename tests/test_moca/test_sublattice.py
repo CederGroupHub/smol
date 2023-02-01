a0ba31ad29acb3b18ad32ca33dc85a3c85caf52b
lbluque <lbluque@berkeley.edu>
2023-01-31 17:03:52 -0800

    add test for sublattice equality

--- tests/test_moca/test_sublattice.py
+++ tests/test_moca/test_sublattice.py
@@ -153,3 +153,19 @@ def test_inactiveness(sublattice, rng):
 
     sublattice.restrict_sites(sublattice.sites)
     assert not sublattice.is_active
+
+def test_equal(sublattice, rng):
+    sublatt2 = Sublattice.from_dict(sublattice.as_dict())
+    assert sublattice == sublatt2
+    sublatt2.restrict_sites(rng.choice(sublatt2.sites, 5, replace=False))
+    assert sublattice == sublatt2
+
+    # change the site_space
+    sublatt2.site_space = SiteSpace(Composition({"A": 0.5}))
+    assert sublattice != sublatt2
+
+    # change the sites
+    sublatt2.site_space = sublattice.site_space
+    sublatt2.reset_restricted_sites()
+    sublatt2.sites = np.arange(len(sublattice.sites) - 5)
+    assert sublattice != sublatt2
