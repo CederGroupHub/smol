dfadead9c5e64799ffce99a667a6b7e8b7a0308b
lbluque <lbluque@berkeley.edu>
2023-04-14 12:13:53 -0700

    MAINT: move solver.py

--- tests/test_solver/test_solver.py
+++ tests/test_solver/test_solver.py
@@ -8,7 +8,7 @@ from pymatgen.analysis.structure_matcher import StructureMatcher
 from pymatgen.core import Lattice, Structure
 
 from smol.capp.generate.groundstate import get_variable_values_from_occupancy
-from smol.capp.generate.groundstate.upper_bound.solver import UpperboundSolver
+from smol.capp.generate.groundstate.solver import UpperboundSolver
 from smol.cofe import ClusterExpansion, ClusterSubspace
 from smol.moca import Ensemble
 from smol.moca.utils.occu import get_dim_ids_table, occu_to_counts
