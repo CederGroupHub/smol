6847283509b8f651429c5eaf6140f69b4a113b58
qchempku2017 <fengyu_xie@lbl.gov>
2023-07-10 11:47:49 -0700

    Fixes flake8 issues.

--- smol/capp/generate/__init__.py
+++ smol/capp/generate/__init__.py
@@ -4,11 +4,11 @@ from smol.capp.generate.enumerate import enumerate_supercell_matrices
 from smol.capp.generate.random import generate_random_ordered_occupancy
 from smol.capp.generate.special.sqs import StochasticSQSGenerator
 
+from .groundstate.solver import GroundStateSolver
+
 __all__ = [
     "enumerate_supercell_matrices",
     "generate_random_ordered_occupancy",
     "StochasticSQSGenerator",
+    "GroundStateSolver",
 ]
-from .groundstate.solver import GroundStateSolver
-
-__all__ = ["GroundStateSolver"]
