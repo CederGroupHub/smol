bd2d8534037b6775aa242cb2d0825f7b5f244e14
Izzy Xie <664222956@qq.com>
2023-07-22 17:40:38 -0700

    DEV: change import order of UpperBoundSolver. DOC: add setting-composition-constraints.ipynb, modify finding-groundstates.ipynb.

--- smol/capp/generate/__init__.py
+++ smol/capp/generate/__init__.py
@@ -1,7 +1,6 @@
 """General tools for structure and occupancy generation."""
 
 from smol.capp.generate.enumerate import enumerate_supercell_matrices
-from smol.capp.generate.groundstate.upper_bound.solver import UpperBoundSolver
 from smol.capp.generate.random import generate_random_ordered_occupancy
 from smol.capp.generate.special.sqs import StochasticSQSGenerator
 
@@ -9,5 +8,4 @@ __all__ = [
     "enumerate_supercell_matrices",
     "generate_random_ordered_occupancy",
     "StochasticSQSGenerator",
-    "UpperBoundSolver",
 ]
--- smol/capp/generate/groundstate/__init__.py
+++ smol/capp/generate/groundstate/__init__.py
@@ -1,8 +1,11 @@
-"""Ground state groundstate functionalities."""
+"""Ground state solver functionalities."""
 from importlib.util import find_spec
+from .upper_bound.solver import UpperBoundSolver
 
 # Check cvxpy installation.
 if find_spec("cvxpy") is None:
     raise ImportError(
         "Ground state utilities require cvxpy and integer-programming solvers!"
     )
+
+__all__ = ["UpperBoundSolver"]
